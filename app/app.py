44c3e338838ed2ef05e13921babfe5e7d8c9908f
mconrado <mconrado@protonmail.com>
2024-09-28 20:27:51 -0300

    owner model

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
-
-
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return "Conexão com o MySQL bem-sucedida!sdaf"
-
+    return "Conexão com o MySQL bem-sucedida!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
