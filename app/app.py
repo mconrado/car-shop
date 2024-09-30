c35841f12f5f92d4485d08c3ba69d169f6a5ee20
mconrado <mconrado@protonmail.com>
2024-09-30 16:17:58 -0300

    rotas get e post para car

--- app/app.py
+++ app/app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.config import Config, TestConfig
 from app.database import db
-from app.routes.owner_routes import owner_bp  # Importa o blueprint
+from app.routes.owner_routes import owner_bp
+from app.routes.car_routes import car_bp
 import os
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 app.register_blueprint(owner_bp)
+app.register_blueprint(car_bp)
 @app.route('/')
 def index():
     return "API CAR SHOP"
---
+++ app/routes/car_routes.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, request, jsonify
+from app.models.car import Car
+from app import db
+
+car_bp = Blueprint('car', __name__)
+
+@car_bp.route('/car', methods=['POST'])
+def create_car():
+    try:
+        data = request.get_json()
+        
+        if 'owner_id' not in data or 'color' not in data or 'model' not in data:
+            return jsonify({"message": "Owner ID, cor e modelo são obrigatórios."}), 400
+        
+        new_car = Car(owner_id=data['owner_id'], color=data['color'], model=data['model'])
+        db.session.add(new_car)
+        db.session.commit()
+        
+        return jsonify({
+            "message": "Carro criado com sucesso.",
+            "id": new_car.id,
+            "owner_id": new_car.owner_id,
+            "color": new_car.color.value,
+            "model": new_car.model.value,
+            "creation_date": new_car.creation_date
+        }), 201
+
+    except Exception as e:
+        return jsonify({"message": "Erro ao criar carro.", "error": str(e)}), 500
+
+@car_bp.route('/car/<int:car_id>', methods=['GET'])
+def get_car(car_id):
+    try:
+        car = Car.query.get(car_id)
+        
+        if car is None:
+            return jsonify({"message": "Carro não encontrado."}), 404
+        
+        return jsonify({
+            "id": car.id,
+            "owner_id": car.owner_id,
+            "color": car.color.value,
+            "model": car.model.value,
+            "creation_date": car.creation_date
+        }), 200
+
+    except Exception as e:
+        return jsonify({"message": "Erro ao buscar carro.", "error": str(e)}), 500
+
