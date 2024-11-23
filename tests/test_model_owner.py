3cedf51937d26fb7fd580df139e3a228a56613f7
mconrado <mconrado@protonmail.com>
2024-11-22 23:22:54 -0300

    refatorando teste model owner

--- tests/test_model_owner.py
+++ tests/test_model_owner.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from app.validators import is_valid_email
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def setup_owner(client):
     with client.application.app_context():
         owner = Owner(name="Márcio Conrado", email="marcio@conrado.com")
@@ -15,20 +15,24 @@ def setup_owner(client):
         db.session.delete(owner)
         db.session.commit()
 
-def test_owner_model_exists(client):
+
+def test_owner_model_exists():
     assert Owner is not None
-    assert hasattr(Owner, '__tablename__')
-    assert hasattr(Owner, 'id')
-    assert hasattr(Owner, 'name')
-    assert hasattr(Owner, 'email')
-    assert hasattr(Owner, 'sales_o')
-    assert hasattr(Owner, 'creation_date')
+    assert hasattr(Owner, "__tablename__")
+    assert hasattr(Owner, "id")
+    assert hasattr(Owner, "name")
+    assert hasattr(Owner, "email")
+    assert hasattr(Owner, "sales_o")
+    assert hasattr(Owner, "creation_date")
+
 
 def test_creation_date(setup_owner):
     assert isinstance(setup_owner.creation_date, datetime)
 
+
 def test_sales_opportunity_default_to_False(setup_owner):
     assert setup_owner.sales_o == False
+
 
 def test_email_field_is_email(setup_owner):
     assert is_valid_email(setup_owner.email)
